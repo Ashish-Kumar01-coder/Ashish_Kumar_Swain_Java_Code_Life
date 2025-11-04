57f728811d804f4f376d457f4b29ab9b03efc49a
Ashish-Kumar01-javacoder <ashishkuswain2002@gmail.com>
2025-11-04 23:36:33 +0530

    My first java program...

---
+++ Add/src/main/java/com/kodewala/practise/Add/App.java
@@ -0,0 +1,13 @@
+package com.kodewala.practise.Add;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
